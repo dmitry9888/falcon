421c8348b6dc281dfdbe614371a16f3c6e56509c
dmitry9888 <githuv@mail.ru>
2022-04-11 12:05:45 +0300

    Ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -36,7 +36,7 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 51835);
+        return MakeUnique<CBaseChainParams>("", 51836);
     else if (chain == CBaseChainParams::TESTNET)
         return MakeUnique<CBaseChainParams>("testnet", 52035);
     else if (chain == CBaseChainParams::REGTEST)
